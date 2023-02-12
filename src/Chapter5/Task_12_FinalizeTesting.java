b30e2cad23cf05de894dd464d241c101e6d5699e
ElnurKasimov <ekasimov2012@gmail.com>
2023-02-12 21:14:49 +0200

    refactored Task12 Chapter 5

--- src/Chapter5/Task_12_FinalizeTesting.java
+++ src/Chapter5/Task_12_FinalizeTesting.java
@@ -24,15 +24,26 @@ class Tank {
 
 }
 
+class Truck {
 
+    void refuel(Tank tank) {
+        if (tank.isFilled) {
+            System.out.println("Ok.Refueled from tank  " + tank);
+        } else {
+            System.out.println("Can not refuel. Fill tank first.");
+        }
+    }
+}
 
 public class Task_12_FinalizeTesting {
 
     public static void main(String[] args) {
         Tank tank = new Tank();
+        Truck truck = new Truck();
+        truck.refuel(tank);
         tank.fill();
+        truck.refuel(tank);
         new Tank().fill();
         System.gc();
-
     }
 }
